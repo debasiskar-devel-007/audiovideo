37091ca50e731d3f7b4f8b0fc8ea3bfa5c63b4d4
Rohit Dey <rohitinfluxiq@gmail.com>
2020-03-15 22:50:39 +0530

    update in video-recorder

--- src/app/video-recorder/video-recorder.component.ts
+++ src/app/video-recorder/video-recorder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import * as RecordRTC from 'recordrtc';
 
 @Component({
   selector: 'app-video-recorder',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoRecorderComponent implements OnInit {
 
+  videoElement: any;  
+  video: any;
+
+  isPlaying = false;
+
+  displayControls = true;
+
   constructor() { }
 
   ngOnInit() {
+	this.video = this.videoElement.nativeElement;
+  }
+
+   start() {
+    this.initCamera({ video: true, audio: false });
+  }
+
+  pause() {
+    this.video.pause();
+  }
+
+  toggleControls() {
+    this.video.controls = this.displayControls;
+    this.displayControls = !this.displayControls;
+  }
+
+  resume() {
+    this.video.play();
+  }
+
+  sound() {
+    this.initCamera({ video: true, audio: true });
+  }
+
+  initCamera(config:any) {
+    var browser = <any>navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      this.video.src = window.URL.createObjectURL(stream);
+      this.video.play();
+    });
   }
 
 }
